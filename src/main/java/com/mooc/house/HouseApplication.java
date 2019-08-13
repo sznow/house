e1aeb3565f8bf04831283144bfa5b72b276faa94
duboxuan <duboxuan_java@163.com>
2019-08-14 07:41:14 +0800

    maven引入jetty:添加spring-boot-start-jetty,并移除starter-tomcat

--- src/main/java/com/mooc/house/HouseApplication.java
+++ src/main/java/com/mooc/house/HouseApplication.java
@@ -1,10 +1,12 @@
-package com.mooc.house.house;
+package com.mooc.house;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-public class HouseApplication {
+@EnableAsync
+public  class HouseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HouseApplication.class, args);
